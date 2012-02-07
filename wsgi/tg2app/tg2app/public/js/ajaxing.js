f217aeab482fb26b266ca061733a852c60ebe2ae
Nathaniel Case <Qalthos@gmail.com>
2012-02-07 11:17:43 -0500

    ooops

--- wsgi/tg2app/tg2app/public/js/ajaxing.js
+++ wsgi/tg2app/tg2app/public/js/ajaxing.js
@@ -5,7 +5,7 @@ $(document).ready(function() {
             var users = json.users;
             $("#sandbox").append("<ul>");
             for ( var i = 0; i < users.length; i++ ) {
-                $("#sandbox").append("<li>" + users[i]i.email + "</li>");
+                $("#sandbox").append("<li>" + users[i].email + "</li>");
             }
             $("#sandbox").append("</ul>");
         },
